4923368ae7b373d0074cf524eef1d37b80d4947e
Simon Schoelly <sischoel@gmail.com>
2020-04-12 23:38:35 +0200

    Restructure import/exports

--- src/SimpleValueGraphs.jl
+++ src/SimpleValueGraphs.jl
@@ -1,71 +1,95 @@
 module SimpleValueGraphs
 
-using LightGraphs
-using LightGraphs.SimpleGraphs: AbstractSimpleGraph, AbstractSimpleEdge,
-    SimpleEdge, IsDirected
+using LightGraphs: AbstractGraph, AbstractEdgeIter, AbstractEdge
 
-using SimpleTraits
+using LightGraphs.SimpleGraphs: AbstractSimpleGraph, AbstractSimpleEdge,
+    SimpleGraph, SimpleDiGraph, SimpleEdge, IsDirected
 
 using Base: OneTo
 
-import Base: eltype, show, reverse, iterate, length, replace_in_print_matrix,
-    getindex, size, zero, Tuple
-
-import SparseArrays: blockdiag
 
 import LightGraphs
 const LG = LightGraphs
 
 import LightGraphs:
-    src, dst, edgetype, nv, ne, vertices, edges, is_directed,
-    add_vertex!, add_edge!, rem_vertex!, rem_edge!,
-    has_vertex, has_edge, inneighbors, outneighbors,
-    indegree, outdegree, degree, has_self_loops, num_self_loops,
-    add_vertex!, adjacency_matrix, weights,
-    SimpleGraph, SimpleDiGraph, IsDirected,
+    nv, ne, is_directed,
+    eltype, vertices, has_vertex,
+    edgetype, edges, src, dst, reverse,
+    weights,
+    add_edge!, rem_edge!, has_edge,
+    inneighbors, outneighbors,
+    zero
 
-    # operators
-    complement
-     
+import Base: show, iterate, length,
+    getindex, size
 
-export AbstractValGraph,
+
+export
     AbstractEdgeValGraph,
-    EdgeValGraph, EdgeValOutDiGraph, EdgeValDiGraph,
-    AbstractValEdge, ValEdge, ValDiEdge,
-    get_edgeval, get_edgeval_or, get_edgevals, get_edgevals_or,
-    set_edgeval!, set_edgevals!,
-    val, vals,
-    outedgevals, inedgevals,
-    outedgevals_for_key, inedgevals_for_key,
-    default_edgeval, edgevals_type,
-    transform_edgevals!,
-    KeyView, keyview,
-    ValMatrix, AdjacencyMatrix,
+    EdgeValGraph,
+    EdgeValOutDiGraph,
+    EdgeValDiGraph,
 
-    # overridden methods from other packages
+    AbstractValEdge,
+    ValEdge,
+    ValDiEdge,
+
+    val,
+    vals,
+
+    default_edgeval,
+
+    edgevals_type,
+
+    get_edgeval,
+    get_edgeval_or,
+    get_edgevals,
+    get_edgevals_or,
+    set_edgeval!,
+    set_edgevals!,
+
+    inedgevals,
+    inedgevals_for_key,
+    outedgevals,
+    outedgevals_for_key,
+
+    AdjacencyMatrix,
+    ValMatrix,
+
+    # overridden methods from LightGraphs
+    nv,
+    ne,
+    is_directed,
+
     eltype,
     vertices,
     has_vertex,
+
     edgetype,
     edges,
-    nv,
-    ne,
     src,
     dst,
+    reverse,
+
+    weights,
+
     add_edge!,
     rem_edge!,
     has_edge,
-    outneighbors,
+
     inneighbors,
-    iterate,
-    reverse,
-    is_directed,
-    weights,
+    outneighbors,
+
     zero,
+
+    # overridden methods from Base
+    iterate,
     length,
-    show,
-    getindex,
     size,
+    getindex,
+    show,
+
+    # overridden methods from LinearAlgebra
     ishermitian,
     adjoint,
     transpose,
