5591b42be2347552f7fc75523da2424c5d23d45e
Simon Schoelly <sischoel@gmail.com>
2020-09-12 00:27:58 +0200

    Add documentation for AbstractTuples

--- src/AbstractTuples.jl
+++ src/AbstractTuples.jl
@@ -1,11 +1,92 @@
 
+"""
+    AbstractTuples
+
+Module that provides additional functionally for `Tuple` and `NamedTuple`
+
+### See also
+[`AbstractTuple`](@ref), [`AbstractNTuple`](@ref), [`NamedNTuple`](@ref), [`tuple_of_types`](@ref)
+"""
 module AbstractTuples
 
 export AbstractTuple, NamedNTuple, AbstractNTuple, tuple_of_types
 
+
+"""
+    AbstractTuple{<: Tuple}
+
+A type `Union` of a `Tuple` and a `NamedTuple`.
+
+Useful for methods that take either a `Tuple` or a `NamedTuple` of some specific types as some parameter.
+
+### See also
+[`AbstractNTuple`](@ref), [`Tuple`](@ref), [`NTuple`](@ref)
+
+### Examples
+
+```jldoctest
+julia> (1, "xyz") isa AbstractTuple{Tuple{Int, String}}
+true
+
+julia> (a=1, b="xyz") isa AbstractTuple{Tuple{Int, String}}
+true
+
+julia> (a=1, b="xyz") isa AbstractTuple
+true
+```
+"""
 const AbstractTuple{T <: Tuple} = Union{<: T, NamedTuple{S, <:T} where S}
 
+
+"""
+    NamedNTuple{N, T}
+
+A `NamedTuple` with `N` arguments of the same type.
+
+Analogue to `NTuple` for `Tuple`.
+
+### See also
+[`AbstractNTuple`](@ref), [`NTuple`](@ref), [`NamedTuple`](@ref)
+
+### Examples
+
+```jldoctest
+julia> (a = 1, b = 2) isa NTuple{2, Int}
+true
+
+julia> (a = 1, b = 2) isa NTuple{2}
+true
+
+julia> (a = 1, b = "xyz") isa NTuple{2, Int}
+false
+```
+"""
 const NamedNTuple{N, T} = NamedTuple{S, NTuple{N, T}} where S
+
+
+"""
+AbstractNTuple{N, T}
+
+A Union of an `NTuple` and a `NamedNTuple`.
+### See also
+[`NTuple`](@ref), [`NamedNTuple`](@ref)
+
+### Examples
+
+```jldoctest
+julia> (1, 2, 3) isa AbstractNTuple{3, Int}
+true
+
+julia> (a=1, b=2, c=3) isa AbstractNTuple{3, Int}
+true
+
+julia> (1, 2) isa AbstractNTuple{3, Int}
+false
+
+julia> (a=1, b=2, c="xyz") isa AbstractNTuple{3, Int}
+false
+```
+"""
 const AbstractNTuple{N, T} = Union{NTuple{N, T}, NamedNTuple{N, T}}
 
 
@@ -15,7 +96,7 @@ const AbstractNTuple{N, T} = Union{NTuple{N, T}, NamedNTuple{N, T}}
 
 Convert a Tuple or NamedTuple type to a Tuple or NamedTuple containing the tuples types.
 
-# Example
+### Examples
 
 ```jldoctest
 julia> tuple_of_types(Tuple{Int64, String})
@@ -36,4 +117,4 @@ function tuple_of_types(TT::Type{<:NamedTuple})
 end
 
 
-end
+end # Module
