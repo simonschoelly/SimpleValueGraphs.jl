95e62608522e7a9731d294275beef4a03ec9d974
Simon Schoelly <sischoel@gmail.com>
2020-09-16 23:55:49 +0200

    Improve documentation for AdjacencyMatrix

--- src/matrices.jl
+++ src/matrices.jl
@@ -11,6 +11,35 @@ using SparseArrays: AbstractSparseMatrix
 A matrix view of a graph.
 
 Entry `(i, j)` is `true` if `i -> j` is and edge of `g` and `false` otherwise.
+
+As this as a view, the entries and the size of this matrix can change
+when the underlying graph changes. The view itself is immutable. Convert
+to a `Matrix` or `SparseMatrixCSC` to get a mutable matrix that does
+not change when the graph does.
+
+### See also
+[`adjacency_matrix`](@ref), [`ValMatrix`](@ref), [`weigths`](@ref)
+
+### Examples
+
+```jldoctest
+julia> g = complete_bipartite_graph(2, 2)
+{4, 4} undirected simple Int64 graph
+
+julia> AdjacencyMatrix(g)
+4×4 AdjacencyMatrix{SimpleGraph{Int64}}:
+ 0  0  1  1
+ 0  0  1  1
+ 1  1  0  0
+ 1  1  0  0
+
+julia> AdjacencyMatrix(g) |> Matrix
+4×4 Array{Bool,2}:
+ 0  0  1  1
+ 0  0  1  1
+ 1  1  0  0
+ 1  1  0  0
+```
 """
 struct AdjacencyMatrix{G <: AbstractGraph} <: AbstractSparseMatrix{Bool, Int}
     graph::G
@@ -26,7 +55,25 @@ end
 """
     adjacency_matrix(g::AbstractEdgeValGraph)
 
-Create an `AdjacencyMatrix` from a graph `g`.
+Create an `AdjacencyMatrix` view from a graph `g`.
+
+### See also
+[`AdjacencyMatrix`](@ref), [`ValMatrix`](@ref), [`weigths`](@ref)
+
+### Examples
+
+```jldoctest
+julia> gv = EdgeValGraph((s, d) -> (1.0, ), star_graph(4), (Float64, ))
+{4, 3} undirected EdgeValGraph{Int64} graph with edge values of type (Float64,).
+
+
+julia> adjacency_matrix(gv)
+4×4 AdjacencyMatrix{EdgeValGraph{Int64,Tuple{Float64},Tuple{Array{Array{Float64,1},1}}}}:
+ 0  1  1  1
+ 1  0  0  0
+ 1  0  0  0
+ 1  0  0  0
+```
 """
 LG.adjacency_matrix(g::AbstractEdgeValGraph) = AdjacencyMatrix(g)
 
@@ -45,7 +92,7 @@ LinearAlgebra.transpose(matrix::AdjacencyMatrix{<:EdgeValGraph}) = matrix
 
 A matrix view of the edge values for a specific key of a graph.
 """
-struct ValMatrix{Tv, G <: AbstractGraph, key} <: AbstractSparseMatrix{Tv, Int} # TODO maybe not int
+struct ValMatrix{Tv, G <: AbstractGraph, key} <: AbstractSparseMatrix{Tv, Int}
 
     graph::G
     zero_value::Tv
@@ -95,11 +142,10 @@ function weights end
 
 LG.weights(g::ZeroEdgeValGraph) = LG.DefaultDistance(nv(g))
 
-LG.weights(g::OneEdgeValGraph; kwargs...) = weights(g, 1; kwargs...)
+LG.weights(g::OneEdgeValGraph; kwargs...) = LG.weights(g, 1; kwargs...)
 
-function LG.weights(g::AbstractEdgeValGraph, key; zerovalue=zero(E_VAL_for_key(g, key)))
+function LG.weights(g::AbstractEdgeValGraph, key; zerovalue=zero(E_VAL_for_key(edgevals_type(g), key)))
 
     return ValMatrix(g, key, zerovalue)
-
 end
 
