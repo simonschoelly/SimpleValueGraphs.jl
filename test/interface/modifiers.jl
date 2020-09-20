7406106ea4a9ce4f8b3f2efd2b848e36db78d9ee
Simon Schoelly <sischoel@gmail.com>
2020-09-20 11:15:38 +0200

    Add tests for rem_edge!

--- test/interface/modifiers.jl
+++ test/interface/modifiers.jl
@@ -68,4 +68,108 @@ end
     @test get_val(g1, 2, 1, :) == (a='y', b=2.0)
 end
 
+@testset "rem_edge! 1" begin
+    g = EdgeValGraph{Int, NamedTuple{(:a,), Tuple{Int}}}(3)
+
+    # removing non existing edge should return false
+    @test !rem_edge!(g, 1, 2)
+    @test !rem_edge!(g, 0, 4) # out of bounds
+    @test ne(g) == 0
+
+    # removing an existing edge
+    add_edge!(g, 1, 2, (a=10,))
+    @test rem_edge!(g, 1, 2)
+    @test !has_edge(g, 1, 2)
+    @test !has_edge(g, 2, 1)
+    @test ne(g) == 0
+    # can't remove twice
+    @test !rem_edge!(g, 1, 2)
+
+    add_edge!(g, 1, 2, (a=20,))
+    add_edge!(g, 3, 3, (a=20,))
+
+    # removing with src, dst interchanged
+    @test rem_edge!(g, 2, 1)
+    @test !has_edge(g, 1, 2)
+    @test !has_edge(g, 2, 1)
+    @test ne(g) == 1
+
+    # remvoing a self-loop
+    @test rem_edge!(g, 3, 3)
+    @test !has_edge(g, 3, 3)
+    @test ne(g) == 0
+end
+
+@testset "rem_edge! 2" begin
+    g = EdgeValDiGraph{Int, Tuple{Float64, Float64}}(4)
+
+    # removing non existing edge should return false
+    @test !rem_edge!(g, 1, 1)
+    @test !rem_edge!(g, -1, 3) # out of bounds
+    @test ne(g) == 0
+
+    # removing an one-directed existing edge
+    add_edge!(g, 1, 2, (1.0, 2.0))
+    @test rem_edge!(g, 1, 2)
+    @test !has_edge(g, 1, 2)
+    @test !has_edge(g, 2, 1)
+    @test ne(g) == 0
+    # can't remove twice
+    @test !rem_edge!(g, 1, 2)
+
+    # removing an two-directed existing edge
+    add_edge!(g, 3, 4, (3.0, 4.0))
+    add_edge!(g, 4, 3, (3.0, 4.0))
+    @test rem_edge!(g, 3, 4)
+    @test !has_edge(g, 3, 4)
+    @test has_edge(g, 4, 3)
+    @test ne(g) == 1
+    # remove other edge
+    @test rem_edge!(g, 4, 3)
+    @test !has_edge(g, 3, 4)
+    @test !has_edge(g, 4, 3)
+    @test ne(g) == 0
+
+end
+
+@testset "rem_edge! 3" begin
+    g = EdgeValOutDiGraph{Int, Tuple{Float64, Float64}}(4)
+
+    # removing non existing edge should return false
+    @test !rem_edge!(g, 1, 1)
+    @test !rem_edge!(g, -1, 3) # out of bounds
+    @test ne(g) == 0
+
+    # removing an one-directed existing edge
+    add_edge!(g, 1, 2, (1.0, 2.0))
+    @test rem_edge!(g, 1, 2)
+    @test !has_edge(g, 1, 2)
+    @test !has_edge(g, 2, 1)
+    @test ne(g) == 0
+    # can't remove twice
+    @test !rem_edge!(g, 1, 2)
+
+    # removing an two-directed existing edge
+    add_edge!(g, 3, 4, (3.0, 4.0))
+    add_edge!(g, 4, 3, (3.0, 4.0))
+    @test rem_edge!(g, 3, 4)
+    @test !has_edge(g, 3, 4)
+    @test has_edge(g, 4, 3)
+    @test ne(g) == 1
+    # remove other edge
+    @test rem_edge!(g, 4, 3)
+    @test !has_edge(g, 3, 4)
+    @test !has_edge(g, 4, 3)
+    @test ne(g) == 0
+
+    # removing a self-loop
+    add_edge!(g, 2, 2, (3.0, 4.0))
+    @test rem_edge!(g, 2, 2)
+    @test !has_edge(g, 2, 2)
+    @test ne(g) == 0
+    @test !rem_edge!(g, 2, 2)
+    @test !has_edge(g, 2, 2)
+    @test ne(g) == 0
+end
+
 end # testset
