66b80cd09958d711f889037edd862661191a2ab8
Simon Schoelly <sischoel@gmail.com>
2020-09-20 17:34:46 +0200

    Add option to select test with a regex

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,21 +7,36 @@ using SimpleValueGraphs.AbstractTuples: tuple_of_types
 
 include("testutils.jl")
 
-tests = [
-    "AbstractTuples.jl",
-    "abstractvaluegraph.jl",
-    "interface/constructors.jl",
-    "interface/edges.jl",
-    "interface/edgeval_accessors.jl",
-    "interface/iterators.jl",
-    "interface/modifiers.jl",
-    "interface/valuegraph.jl",
-    "matrices.jl",
-]
+tests = Dict{String, String}()
+
+function register_test(path::String)
+
+    tests[path] = path
+end
+
+register_test("AbstractTuples.jl")
+register_test("abstractvaluegraph.jl")
+register_test("interface/constructors.jl")
+register_test("interface/edges.jl")
+register_test("interface/edgeval_accessors.jl")
+register_test("interface/iterators.jl")
+register_test("interface/modifiers.jl")
+register_test("interface/valuegraph.jl")
+register_test("matrices.jl")
+
 
 @testset "ValueGraphs" begin
-    for (i, test) in enumerate(tests)
-        println("$i / $(length(tests)) $test")
-        @time include(test)
+
+    regex = r""
+    if !isempty(ARGS)
+        length(ARGS) > 1 && error("At most one regex can be specified for test_args")
+        regex = Regex(ARGS[1])
+    end
+
+    filtered_test_names = filter(test_name -> occursin(regex, test_name), keys(tests)) |> collect |> sort
+
+    for (i, test_name) in enumerate(filtered_test_names)
+        println("$i / $(length(filtered_test_names)) $test_name")
+        @time include(tests[test_name])
     end
 end # testset
