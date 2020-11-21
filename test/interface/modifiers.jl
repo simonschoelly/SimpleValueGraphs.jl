703db695c82b42c0afc782b5dab21c2e98442d26
Simon Schoelly <sischoel@gmail.com>
2020-11-21 12:00:09 +0100

    Get tests for modifiers to work again

--- test/interface/modifiers.jl
+++ test/interface/modifiers.jl
@@ -2,30 +2,30 @@
 
 @testset "add_edge! 1" begin
 
-    g1 = ValGraph{Int, Tuple{Int}}(3)
+    g1 = ValGraph{Int, Tuple{}, Tuple{Int}}(3)
     # adding first edge succeed
     @test add_edge!(g1, 1, 2, (10, ))
     @test ne(g1) == 1
-    @test get_val(g1, 1, 2, 1) == 10
-    @test get_val(g1, 2, 1, 1) == 10
+    @test get_edgeval(g1, 1, 2, 1) == 10
+    @test get_edgeval(g1, 2, 1, 1) == 10
 
     # adding same edge with different value returns false but changes value
     @test !add_edge!(g1, 1, 2, (20, ))
     @test ne(g1) == 1
-    @test get_val(g1, 1, 2, 1) == 20
-    @test get_val(g1, 2, 1, 1) == 20
+    @test get_edgeval(g1, 1, 2, 1) == 20
+    @test get_edgeval(g1, 2, 1, 1) == 20
 
     # adding a self-loop works
     @test add_edge!(g1, 1, 1, (30, ))
     @test ne(g1) == 2
-    @test get_val(g1, 1, 1, 1) == 30
-    @test get_val(g1, 1, 1, 1) == 30
+    @test get_edgeval(g1, 1, 1, 1) == 30
+    @test get_edgeval(g1, 1, 1, 1) == 30
 
     # another edge, this time higher vertex first
     @test add_edge!(g1, 3, 2, (40, ))
     @test ne(g1) == 3
-    @test get_val(g1, 2, 3, 1) == 40
-    @test get_val(g1, 3, 2, 1) == 40
+    @test get_edgeval(g1, 2, 3, 1) == 40
+    @test get_edgeval(g1, 3, 2, 1) == 40
 
     # adding an edge outside of boundaries does nothing
     @test !add_edge!(g1, 1, 4, (50, ))
@@ -38,54 +38,54 @@ end
 
 @testset "add_edge! 2" begin
 
-    g1 = ValDiGraph{Int, NamedTuple{(:a, :b), Tuple{Char, Float64}}}(4)
+    g1 = ValDiGraph{Int, Tuple{}, NamedTuple{(:a, :b), Tuple{Char, Float64}}}(4)
     # adding first edge succeed
     @test add_edge!(g1, 1, 2, (a='x', b=1.0))
     @test ne(g1) == 1
-    @test get_val(g1, 1, 2, :) == (a='x', b=1.0)
+    @test get_edgeval(g1, 1, 2, :) == (a='x', b=1.0)
     @test !has_edge(g1, 2, 1)
 
     # adding the reverse edge should not change the previous one
     @test add_edge!(g1, 2, 1, (a='y', b=2.0))
     @test ne(g1) == 2
-    @test get_val(g1, 1, 2, :) == (a='x', b=1.0)
-    @test get_val(g1, 2, 1, :) == (a='y', b=2.0)
+    @test get_edgeval(g1, 1, 2, :) == (a='x', b=1.0)
+    @test get_edgeval(g1, 2, 1, :) == (a='y', b=2.0)
 
     # adding an edge twice should return false change the value the second time
     @test add_edge!(g1, 1, 3, (a='z', b=3.0))
     @test ne(g1) == 3
-    @test get_val(g1, 1, 3, :) == (a='z', b=3.0)
+    @test get_edgeval(g1, 1, 3, :) == (a='z', b=3.0)
     @test !add_edge!(g1, 1, 3, (a='z', b=4.0))
     @test ne(g1) == 3
-    @test get_val(g1, 1, 3, :) == (a='z', b=4.0)
+    @test get_edgeval(g1, 1, 3, :) == (a='z', b=4.0)
 end
 
 @testset "add_edge! 3" begin
 
-    g1 = ValOutDiGraph{Int, NamedTuple{(:a, :b), Tuple{Char, Float64}}}(4)
+    g1 = ValOutDiGraph{Int, Tuple{}, NamedTuple{(:a, :b), Tuple{Char, Float64}}}(4)
     # adding first edge succeed
     @test add_edge!(g1, 1, 2, (a='x', b=1.0))
     @test ne(g1) == 1
-    @test get_val(g1, 1, 2, :) == (a='x', b=1.0)
+    @test get_edgeval(g1, 1, 2, :) == (a='x', b=1.0)
     @test !has_edge(g1, 2, 1)
 
     # adding the reverse edge should not change the previous one
     @test add_edge!(g1, 2, 1, (a='y', b=2.0))
     @test ne(g1) == 2
-    @test get_val(g1, 1, 2, :) == (a='x', b=1.0)
-    @test get_val(g1, 2, 1, :) == (a='y', b=2.0)
+    @test get_edgeval(g1, 1, 2, :) == (a='x', b=1.0)
+    @test get_edgeval(g1, 2, 1, :) == (a='y', b=2.0)
 
     # adding an edge twice should return false change the value the second time
     @test add_edge!(g1, 1, 3, (a='z', b=3.0))
     @test ne(g1) == 3
-    @test get_val(g1, 1, 3, :) == (a='z', b=3.0)
+    @test get_edgeval(g1, 1, 3, :) == (a='z', b=3.0)
     @test !add_edge!(g1, 1, 3, (a='z', b=4.0))
     @test ne(g1) == 3
-    @test get_val(g1, 1, 3, :) == (a='z', b=4.0)
+    @test get_edgeval(g1, 1, 3, :) == (a='z', b=4.0)
 end
 
 @testset "rem_edge! 1" begin
-    g = ValGraph{Int, NamedTuple{(:a,), Tuple{Int}}}(3)
+    g = ValGraph{Int, Tuple{}, NamedTuple{(:a,), Tuple{Int}}}(3)
 
     # removing non existing edge should return false
     @test !rem_edge!(g, 1, 2)
@@ -110,14 +110,14 @@ end
     @test !has_edge(g, 2, 1)
     @test ne(g) == 1
 
-    # remvoing a self-loop
+    # removing a self-loop
     @test rem_edge!(g, 3, 3)
     @test !has_edge(g, 3, 3)
     @test ne(g) == 0
 end
 
 @testset "rem_edge! 2" begin
-    g = ValDiGraph{Int, Tuple{Float64, Float64}}(4)
+    g = ValDiGraph{Int, Tuple{}, Tuple{Float64, Float64}}(4)
 
     # removing non existing edge should return false
     @test !rem_edge!(g, 1, 1)
@@ -149,7 +149,7 @@ end
 end
 
 @testset "rem_edge! 3" begin
-    g = ValOutDiGraph{Int, Tuple{Float64, Float64}}(4)
+    g = ValOutDiGraph{Int, Tuple{}, Tuple{Float64, Float64}}(4)
 
     # removing non existing edge should return false
     @test !rem_edge!(g, 1, 1)
