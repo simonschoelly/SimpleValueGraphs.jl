8553a60b0f61622959d8c82e08ef3e99ee54d7cd
Simon Schoelly <sischoel@gmail.com>
2021-01-24 22:49:24 +0100

    Fix incorrect docstring output

--- src/matrices.jl
+++ src/matrices.jl
@@ -178,7 +178,7 @@ julia> gv = ValDiGraph(path_digraph(3),  edgeval_types=(a=Float64, b=String), ed
    graph value types: ()
 
 julia> ValMatrix(gv, 1, 0.0)
-3×3 ValMatrix{Float64,ValDiGraph{Int64,Tuple{},NamedTuple{(:a, :b),Tuple{},Tuple{Float64,String}},Tuple{},NamedTuple{(:a, :b),Tuple{Array{Array{Float64,1},1},Array{Array{String,1},1}}}},1}:
+3×3 ValMatrix{Float64,ValDiGraph{Int64,Tuple{},NamedTuple{(:a, :b),Tuple{Float64,String}},Tuple{},Tuple{},NamedTuple{(:a, :b),Tuple{Array{Array{Float64,1},1},Array{Array{String,1},1}}}},1}:
  0.0  0.823648  0.0
  0.0  0.0       0.823648
  0.0  0.0       0.0
