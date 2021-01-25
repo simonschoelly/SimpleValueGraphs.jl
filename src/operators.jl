b26136ef9137aa9c50c15d5da2adf5f5aa3870b3
Simon Schoelly <sischoel@gmail.com>
2021-01-25 22:05:55 +0100

    Add documentation

--- src/operators.jl
+++ src/operators.jl
@@ -2,6 +2,16 @@
 
 const CANDIDATE_SQUASH_TYPES = [Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64]
 
+"""
+    squash(g::ValGraph)
+    squash(g::ValDiGraph)
+    squash(g::ValOutDiGraph)
+
+Return a copy of `g` with an `eltype` as small as possible.
+
+This can help with performance. Only the `eltype` is changed, all other
+types stay the same.
+"""
 function squash(g::Union{ValGraph, ValDiGraph, ValOutDiGraph})
 
     nvg = nv(g)
@@ -12,3 +22,5 @@ function squash(g::Union{ValGraph, ValDiGraph, ValOutDiGraph})
         nvg < typemax(V) && return G{V}(g)
     end
 end
+
+# TODO implement destructive squash!
