6c25bcd215a80c6b85641321db329d45397c2d65
Simon Schoelly <sischoel@gmail.com>
2021-02-26 02:08:56 +0100

    Add reverse operator

--- src/operators.jl
+++ src/operators.jl
@@ -1,4 +1,7 @@
 
+# ==================================
+# squash
+# ==================================
 
 const CANDIDATE_SQUASH_TYPES = [Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64]
 
@@ -24,3 +27,72 @@ function squash(g::Union{ValGraph, ValDiGraph, ValOutDiGraph})
 end
 
 # TODO implement destructive squash!
+
+# ==================================
+#    reverse
+# ==================================
+
+"""
+    Reverse
+
+Lazy wrapper for value graphs that reverses the directions of the graphs edges.
+If this object is mutated, the wrapped graph is mutated as well.
+"""
+struct Reverse{V, V_VALS, E_VALS, G_VALS, G<:AbstractValGraph{V, V_VALS, E_VALS, G_VALS}} <: AbstractValGraph{V, V_VALS, E_VALS, G_VALS}
+
+    graph::G
+end
+
+_graphtype(::Type{<:Reverse{V, V_VALS, E_VALS, G_VALS, G}}) where {V, V_VALS, E_VALS, G_VALS, G} = G
+
+"""
+    reverse(g::AbstractValGraph)
+
+Reverse the direction of the edges of `g`. Can also return `Reverse(g)` - a lazy
+wrapper around `g`.
+"""
+reverse(g::AbstractValGraph) = is_directed(g) ? Reverse(g) : g
+
+reverse(rg::Reverse) = rg.graph
+
+## ---------------------------------
+##   Reverse
+## ---------------------------------
+
+nv(rg::Reverse) = nv(rg.graph)
+
+has_edge(rg::Reverse, s::Integer, d::Integer) =  has_edge(rg.graph, d, s)
+
+LG.is_directed(RG::Type{<:Reverse}) = is_directed(_graphtype(RG))
+
+zero(RG::Type{<:Reverse}) = Reverse{_graphtype(RG)}(zero(RG))
+
+get_vertexval(rg::Reverse, v::Integer, key::Integer) = get_vertexval(rg.graph, v, key)
+get_vertexval(rg::Reverse, v::Integer, key::Symbol) = get_vertexval(rg.graph, v, key)
+get_vertexval(rg::Reverse, v::Integer, ::Colon) = get_vertexval(rg.graph, v, :)
+
+set_vertexval!(rg::Reverse, v::Integer, key::Integer, value) = set_vertexval!(rg.graph, v, key, value)
+set_vertexval!(rg::Reverse, v::Integer, key::Symbol, value) = set_vertexval!(rg.graph, v, key, value)
+set_vertexval!(rg::Reverse, v::Integer, ::Colon, values) = set_vertexval!(rg.graph, v, :, values)
+
+get_edgeval(rg::Reverse, s::Integer, d::Integer, key::Integer) = get_edgeval(rg.graph, d, s, key)
+get_edgeval(rg::Reverse, s::Integer, d::Integer, key::Symbol) = get_edgeval(rg.graph, d, s, key)
+get_edgeval(rg::Reverse, s::Integer, d::Integer, ::Colon) = get_edgeval(rg.graph, d, s, :)
+
+set_edgeval!(rg::Reverse, s::Integer, d::Integer, key::Integer, value) = set_edgeval!(rg.graph, d, s, key, value)
+set_edgeval!(rg::Reverse, s::Integer, d::Integer, key::Symbol, value) = set_edgeval!(rg.graph, d, s, key, value)
+set_edgeval!(rg::Reverse, s::Integer, d::Integer, ::Colon, values) = set_edgeval!(rg.graph, d, s, :, values)
+
+get_graphval(rg::Reverse, key::Integer) = get_graphval(rg.graph, key)
+get_graphval(rg::Reverse, key::Symbol) = get_graphval(rg.graph, key)
+get_graphval(rg::Reverse, ::Colon) = get_graphval(rg.graph, :)
+
+set_graphval!(rg::Reverse, key::Integer, value) = set_graphval!(rg.graph, key, value)
+set_graphval!(rg::Reverse, key::Symbol, value) = set_graphval!(rg.graph, key, value)
+set_graphval!(rg::Reverse, ::Colon, values) = set_graphval!(rg.graph, :, values)
+
+add_vertex!(rg::Reverse, values) = add_vertex!(rg.graph, values)
+
+add_edge!(rg::Reverse, s::Integer, d::Integer, values) = add_edge!(rg.graph, d, s, values)
+
+ne(rg::Reverse) = ne(rg.graph)
--- test/testutils.jl
+++ test/testutils.jl
@@ -401,3 +401,47 @@ end
 allkeys_for_E_VALS(E_VALS::Type{<:Tuple}) = 1:length(E_VALS.types)
 allkeys_for_E_VALS(E_VALS::Type{<:NamedTuple}) = E_VALS.names ∪
                                                  (1:length(E_VALS.names))
+
+
+# ========================================
+#    DummyValGraph
+# ========================================
+
+using SimpleValueGraphs: AbstractValGraph
+
+struct DummyValGraph{V, V_VALS, E_VALS, G_VALS, G <: AbstractValGraph{V, V_VALS, E_VALS, G_VALS}} <: AbstractValGraph{V, V_VALS, E_VALS, G_VALS}
+
+    wrapped::G
+end
+
+SimpleValueGraphs.nv(g::DummyValGraph) = nv(g.wrapped)
+
+SimpleValueGraphs.is_directed(::Type{<:DummyValGraph{V, V_VALS, E_VALS, G_VALS, G}}) where {V, V_VALS, E_VALS, G_VALS, G} = is_directed(G)
+
+SimpleValueGraphs.has_edge(g::DummyValGraph, s, d) = has_edge(g.wrapped, s, d)
+
+SimpleValueGraphs.get_edgeval(g::DummyValGraph, s, d, key::Integer) =
+    get_edgeval(g.wrapped, s, d, key)
+
+SimpleValueGraphs.get_vertexval(g::DummyValGraph, v, key::Integer) =
+    get_vertexval(g.wrapped, v, key)
+
+SimpleValueGraphs.set_vertexval!(g::DummyValGraph, v, key::Integer, value) =
+    set_vertexval!(g.wrapped, v, key, value)
+
+SimpleValueGraphs.get_edgeval(g::DummyValGraph, s, d, key::Integer) =
+    get_edgeval(g.wrapped, s, d, key)
+
+SimpleValueGraphs.set_edgeval!(g::DummyValGraph, s, d, key::Integer, value) =
+    set_edgeval!(g.wrapped, s, d, key, value)
+
+SimpleValueGraphs.get_graphval(g::DummyValGraph, key::Integer) =
+    get_graphval(g.wrapped, key)
+
+SimpleValueGraphs.set_graphval!(g::DummyValGraph, key::Integer, value) =
+    set_graphval!(g.wrapped, key, value)
+
+SimpleValueGraphs.add_vertex!(g::DummyValGraph, values) = add_vertex!(g.wrapped, values)
+
+SimpleValueGraphs.add_edge!(g::DummyValGraph, s, d, values) = add_edge!(g.wrapped, s, d, values)
+
